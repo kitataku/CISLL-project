d0b032058548a5245ede0c459b5a4c5ad515c0d3
kitataku <kitataku764@gmial.com>
2021-12-30 16:53:22 +0900

    add comment

--- getbot.py
+++ getbot.py
@@ -5,8 +5,8 @@ from bots.adx import ADX
 def get_bot(bot_name, params):
     """
     :param bot_name: BOT class name
-    - BollingerBand
-    - ADX
+    - BollingerBand: 逆張りボリンジャーバンド
+    - ADX: ADXによる判断
     :param params: parameters of bot
     """
     bot = None
