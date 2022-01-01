af77797d8092de49c2fccc45c522d4ff3042c2d3
kitataku <kitataku764@gmial.com>
2022-01-01 21:10:01 +0900

    A2C

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from getbot import get_bot
 
 if __name__ == "__main__":
     params = {"pair": "bat_jpy", "test_start": "20211224", "start_jpy": 10000}
-    bot = get_bot(bot_name="ADX", params=params)
+    bot = get_bot(bot_name="A2C", params=params)
     bot.test_run()  # テスト実行
     bot.print_asset()  # 最終結果を出力
